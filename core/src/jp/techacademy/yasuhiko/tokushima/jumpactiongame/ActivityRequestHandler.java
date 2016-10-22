490b40aa2009459a20892c1cc2035b198c758ec4
koutaro <julianseed@gmail.com>
2016-10-23 01:10:38 +0900

    Lesson7課題前の状態

---
+++ core/src/jp/techacademy/yasuhiko/tokushima/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.yasuhiko.tokushima.jumpactiongame;
+
+/**
+ * Created by tokushima on 2016/10/22.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
